bb0ac1b757a1c6cd15d4ef309bd681e7d1c59b20
Wojciech Kulik <wkulik91@gmail.com>
2020-01-26 01:26:27 +0100

    Added support for Apache license

--- Program.cs
+++ Program.cs
@@ -169,9 +169,13 @@ namespace NugetAcknowledgementExporter
             using var httpClient = new HttpClient();
             var lowerLicenseUrl = package.LicenseUrl.ToLowerInvariant();
 
-            if (lowerLicenseUrl.Contains("licenses.nuget.org/mit"))
+            if (lowerLicenseUrl.Contains("licenses.nuget.org/mit") || lowerLicenseUrl.Contains("opensource.org/licenses/mit"))
             {
                 package.License = MITLicense(package.Authors);
+            }
+            else if (lowerLicenseUrl.Contains("apache.org/licenses/license-2.0"))
+            {
+                package.License = ApacheLicense(package.Authors);
             }
             else if (!lowerLicenseUrl.Contains("github.com") &&
                      !lowerLicenseUrl.EndsWith(".txt") &&
@@ -245,6 +249,23 @@ NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
 LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
+        }
+
+        static string ApacheLicense(string authors)
+        {
+            return $@"Copyright {authors}
+
+Licensed under the Apache License, Version 2.0 (the ""License"");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an ""AS IS"" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.";
         }
     }
 }
